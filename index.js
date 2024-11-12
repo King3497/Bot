fcbb434d8e2b025d23680b3a80ce3e3260a92f20
King <yakuzatv0@gmail.com>
2024-11-12 19:02:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const bedrock = require('bedrock-protocol')
-const client = bedrock.createClient({
-  host: 'azurite.magmanode.com',   // optional
-  port: 30578,         // optional, default 19132
-  username: 'Entidade 303',   // the username you want to join as, optional if online mode
-  offline: true       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+const server = bedrock.createServer({
+  host: '162.55.199.41',       // optional. host to bind as.
+  port: 30578,          // optional
+  username: 'Entidade 303',
+  version: '1.21.31',   // optional. The server version, latest if not specified. 
 })
 
-client.on('text', (packet) => { // Listen for chat messages from the server and echo them back.
-  if (packet.source_name != client.username) {
-    client.queue('text', {
-      type: 'chat', needs_translation: false, source_name: client.username, xuid: '', platform_chat_id: '', filtered_message: '',
-      message: `${packet.source_name} said: ${packet.message} on ${new Date().toLocaleString()}`
-    })
-  }
+server.on('connect', client => {
+  client.on('join', () => { // The client has joined the server.
+    const d = new Date()  // Once client is in the server, send a colorful kick message
+    client.disconnect(`Good ${d.getHours() < 12 ? '§emorning§r' : '§3afternoon§r'} :)\n\nMy time is ${d.toLocaleString()} !`)
+  })
 })
